1ca08b2c156fe2ef48cd22869c2e9256db27244f
Ronald Paolo Ramirez <62285588+rptramirez2020@users.noreply.github.com>
2020-03-17 19:12:42 +0800

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Im adding code to my project");
+		System.out.println("Adding code from GitHub");
 	}
 }
